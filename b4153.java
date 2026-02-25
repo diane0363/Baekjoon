cdcbc5d259273597cfd9c82884580ad78afaf1b8
diane0363 <daink0363@gmail.com>
2026-02-25 18:32:36 +0900

    .

---
+++ b4153.java
@@ -0,0 +1,27 @@
+/*
+과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+
+[입력]
+입력은 여러개의 테스트케이스로 주어지며 마지막줄에는 0 0 0이 입력된다. 각 테스트케이스는 모두 30,000보다 작은 양의 정수로 주어지며, 각 입력은 변의 길이를 의미한다.
+
+[출력]
+각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b4153 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] sl = br.readLine().split(" ");
+
+        // pop() 은 스택이나 덱에서 사용 가능. 배열이나 리스트는 사용 불가능
+        System.out.println();
+
+    }
+}
+
